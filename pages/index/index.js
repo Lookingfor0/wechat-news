2d0ce62bd38efe2c4cdc93e3ea83a24d6b4de7ab
NiHao <supernihaooo@gmail.com>
2018-09-17 21:01:54 +0800

    完成导航栏布局

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,11 +2,15 @@
 
 Page({
   data: {
-    
+    currentCat: "gn",
   },
   //事件处理函数
   onLoad: function () {
     
   },
-  
+  //切换新闻分类
+  changeCat: function(event) {
+    let cat = event.target.dataset.cat
+    //.....未完待续
+  },
 })
