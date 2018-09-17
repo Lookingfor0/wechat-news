db03f1c3d78fb8b48fbc845705d9f1bb6e65a072
NiHao <supernihaooo@gmail.com>
2018-09-17 21:35:40 +0800

    实现导航栏样式动态变化

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,16 +1,18 @@
 //index.js
-
 Page({
   data: {
     currentCat: "gn",
   },
   //事件处理函数
   onLoad: function () {
-    
+  //获取新闻...   
   },
   //切换新闻分类
   changeCat: function(event) {
     let cat = event.target.dataset.cat
-    //.....未完待续
+    this.setData({
+      currentCat: cat
+    })
+    //获取新闻...
   },
 })
