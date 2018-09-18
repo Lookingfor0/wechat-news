216cedbcc7b35bc69d38d054a23a7d9cf198c33b
NiHao <supernihaooo@gmail.com>
2018-09-18 19:03:16 +0800

    使用wx:for动态生成新闻种类

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,16 @@
 //index.js
 Page({
   data: {
+    //新闻类别 {代号， 名称}， 可自主添加或删除
+    categories: [
+      {cat: 'gn', name: '国内'}, 
+      {cat: 'gj', name: '国际'},
+      {cat: 'cj', name: '财经'},
+      {cat: 'yl', name: '娱乐'},
+      {cat: 'js', name: '军事'},
+      {cat: 'ty', name: '体育'}, 
+      {cat: 'other', name: '其他'},
+    ],    
     currentCat: "gn",
   },
   //事件处理函数
