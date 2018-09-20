cdb474499bd2501355ea3f405405011a80c92a64
NiHao <supernihaooo@gmail.com>
2018-09-20 18:32:03 +0800

    完成新闻详情页

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -21,7 +21,14 @@ Page({
       data: {id: id},
       success: res => {
         let detail = res.data.result
-        console.log(detail)
+        // 处理时间（日期还是小时分钟）
+        let today = new Date()
+        today.setDate(today.getDate())
+        let date = new Date(detail.date)
+        if (Number(date - today) < 0)
+          detail.date = date.getMonth() + "月" + date.getDate() + "日"
+        else 
+          detail.date = date.getHours() + ":" + date.getMinutes()
         this.setData({
           detail: detail
         })
